b20778a95f8b522a9672d9a4a29589ba5d86a105
Andres Araujo <a.araujo.azua@gmail.com>
2015-08-08 15:21:06 -0500

    chore: update to alpha 34

--- web/main.dart
+++ web/main.dart
@@ -7,19 +7,10 @@ import 'package:angular2/bootstrap.dart';
 import 'package:angular2/di.dart';
 import 'package:angular2/router.dart';
 
-// These imports will go away soon:
-import 'package:angular2/src/reflection/reflection.dart' show reflector;
-import 'package:angular2/src/reflection/reflection_capabilities.dart'
-    show ReflectionCapabilities;
-
 import 'package:ng2_hackernews/ng2_hackernews.dart';
 import 'package:ng2_hackernews/pipes/domain_pipe.dart';
 
 main() {
-
-  // Temporarily needed.
-  reflector.reflectionCapabilities = new ReflectionCapabilities();
-
   var pipeMap = {
     'domain': [ new DomainPipe(), const NullPipeFactory()],
     "iterableDiff": defaultPipes.config['iterableDiff'] //temporaly needed
